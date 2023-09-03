321ae2bef51627b9fbe32f13fba2114f0c7272b7
Steve Calla <callasteven@gmail.com>
2023-09-03 10:16:42 -0600

    Add hover to the copy icon

--- frontend/src/components/ViewItem.js
+++ frontend/src/components/ViewItem.js
@@ -1,12 +1,31 @@
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Accordion from "react-bootstrap/Accordion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClipboard } from "@fortawesome/free-regular-svg-icons";
 
-function ViewItem({eventKeyProp, copyToClipboard, copyString, isClickable, copiedAll, timeRemaining, buttonContent, buttonClicked }) {
+function ViewItem({
+  eventKeyProp,
+  copyToClipboard,
+  copyString,
+  isClickable,
+  copiedAll,
+  timeRemaining,
+  buttonContent,
+  buttonClicked,
+}) {
+  const [isHovering, setIsHovering] = useState(false);
 
   const stopPropagation = (event) => {
     event.stopPropagation(); //prevents accordion from opening when clipboard is clicked
+  };
+
+  //HOVER STYLE
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
   };
 
   return (
@@ -20,22 +39,22 @@ function ViewItem({eventKeyProp, copyToClipboard, copyString, isClickable, copie
             stopPropagation(event); //prevents accordion from opening when clipboard is clicked
             isClickable && copyToClipboard(copyString, buttonClicked);
           }}
+          onMouseEnter={() => setIsHovering(true)}
+          onMouseLeave={() => setIsHovering(false)}
           style={{
             position: "absolute",
             left: "235px",
-            color: "gray",
+            color: isHovering && isClickable ? "green" : "gray",
             backgroundColor: "white",
             zIndex: "5",
-            transform: "scale(1.2)",
+            transform: isHovering && isClickable && "scale(1.5)",
             cursor: "pointer",
           }}
         />
         {copiedAll ? `Copied! ${timeRemaining}` : buttonContent}
       </Accordion.Header>
       <Accordion.Body style={{ overflow: "auto", height: "150px" }}>
-        {copyString === "[]"
-          ? "No Data Loaded"
-          : copyString}
+        {copyString === "[]" ? "No Data Loaded" : copyString}
       </Accordion.Body>
     </Accordion.Item>
   );
